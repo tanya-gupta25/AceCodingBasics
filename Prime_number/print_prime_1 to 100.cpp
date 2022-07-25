70bf0d70f2d83e373b473b5380b828cfdb7aad4c
tanya-gupta25 <gtanya729@gmail.com>
2022-07-25 16:38:49 +0530

    Added print prime 1-100

---
+++ Prime_number/print_prime_1 to 100.cpp
@@ -0,0 +1,71 @@
+
+// #Approach1
+
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int isprime(int i)
+{
+    if (i <= 1)
+        return 0;
+    for (int j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    int num, last;
+    cout << "Enter first number" << endl;
+    cin >> num;
+    cout << "Enter last number" << endl;
+    cin >> last;
+    // for (int i = num; i <= 100; i++)
+    for (int i = num; i <= last; i++)
+    {
+        if (isprime(i) == 1)
+            cout << i << endl;
+    }
+    return 0;
+}
+
+// #Approach2
+
+int main()
+{
+    for(int i=1;i<=100;i++)
+    {
+        int count=0;
+        for(int j=2;j<i;j++)
+        {
+            if(i%j==0)
+            {
+                count++;
+            }
+        }
+        if(count==0 & i!=1) cout<<i<<endl;
+    }
+}
+
+// #Approach3
+
+int main()
+{
+    for(int i=1;i<=100;i++)
+    {
+        int count=0;
+        for(int j=2;j<=sqrt(i);j++)
+        {
+            if(i%j==0)
+            {
+                count++;
+            }
+        }
+        if(count==0 && i!=1) cout<<i<<endl;
+    }
+}
